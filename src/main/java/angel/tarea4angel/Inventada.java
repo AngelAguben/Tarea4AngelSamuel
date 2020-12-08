12fe9e5ef037414c7a3c6fb9b3359c42551491a9
Angel <angelbmx002@gmail.com>
2020-12-08 20:25:42 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/angel/tarea4angel/Inventada.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package angel.tarea4angel;
+
+/**
+ *
+ * @author Ángel
+ */
+public class Inventada {
+    int a,b,c;
+}
