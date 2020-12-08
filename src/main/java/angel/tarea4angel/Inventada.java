9a769f126bdd99863f9e6382d7640126d38bbf62
Angel <angelbmx002@gmail.com>
2020-12-08 20:43:32 +0100

    Añade getters y setters en clase inventada

--- src/main/java/angel/tarea4angel/Inventada.java
+++ src/main/java/angel/tarea4angel/Inventada.java
@@ -20,5 +20,30 @@ public class Inventada {
 
     public Inventada() {
     }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+    
     
 }
