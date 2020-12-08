9ec485f6fc5b3195ed9d57915397577431b51030
samuel <samubp10@gmail.com>
2020-12-08 20:46:40 +0100

    Añade una clase con el método main

---
+++ src/main/java/angel/tarea4angel/PruebaInventada.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package angel.tarea4angel;
+
+/**
+ *
+ * @author Samuel
+ */
+public class PruebaInventada {
+    public static void main(String[] args) {
+        
+    }
+}
